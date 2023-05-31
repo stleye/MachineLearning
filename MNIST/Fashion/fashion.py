875f801821ee8d7f6798ba10cfd0d85d59e8e291
Sebastian Tleye <stleye@gmail.com>
2023-05-30 22:10:40 -0300

    Added Fashiong Training Network

--- MNIST/Fashion/fashion.py
+++ MNIST/Fashion/fashion.py
@@ -1,7 +1,9 @@
 import torch
 from torchvision import datasets, transforms
+from torch import nn
 import matplotlib.pyplot as plt
 import helper
+from torch import optim
 
 # Define a transform to normalize the data
 transform = transforms.Compose([transforms.ToTensor(),
@@ -15,7 +17,45 @@ trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
 testset = datasets.FashionMNIST('~/.pytorch/F_MNIST_data/', download=True, train=False, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=True)
 
-image, label = next(iter(trainloader))
-helper.imshow(image[0,:]);
+model = nn.Sequential(nn.Linear(784, 128),
+                      nn.ReLU(),
+                      nn.Linear(128, 64),
+                      nn.ReLU(),
+                      nn.Linear(64, 10), 
+                      nn.LogSoftmax(dim=1))
+
+criterion = nn.NLLLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.003)
+epochs = 5
+
+for e in range(epochs):
+    running_loss = 0
+    for images, labels in trainloader:
+        # Flatten MNIST images into a 784 long vector
+        images = images.view(images.shape[0], -1)
+    
+        # TODO: Training pass
+        
+        optimizer.zero_grad()
+        output = model.forward(images)
+        loss = criterion(output, labels)
+        loss.backward()
+        optimizer.step()
+        
+        running_loss += loss.item()
+    else:
+        print(f"Training loss: {running_loss/len(trainloader)}")
+
+dataiter = iter(testloader)
+images, labels = next(dataiter)
+img = images[0]
+# Convert 2D image to 1D vector
+img = img.resize_(1, 784)
+
+# TODO: Calculate the class probabilities (softmax) for img
+ps = torch.exp(model(img))
+
+# Plot the image and probabilities
+helper.view_classify(img.resize_(1, 28, 28), ps, version='Fashion')
 
 plt.show()
